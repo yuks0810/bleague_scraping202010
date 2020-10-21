0d825b62a762cec5a5123d07d0b38ef86d131bd3
yuki.nakata <dorcushopeino1@gmail.com>
2020-10-21 14:30:25 +0900

    いらないコメント削除

--- main/bleague_scrape.py
+++ main/bleague_scrape.py
@@ -1,16 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# target_url = 'https://www.bleague.jp/schedule/?tab=1&year=2020&event=2'
-# r = requests.get(target_url)
-
-# soup = BeautifulSoup(r.text, 'lxml')
-
-# # for a in soup.find_all('a'):
-# #       print(a.get('href'))
-
-# print(soup)
-
 import time
 # import classes
 from browser_controll import BrowserControll
